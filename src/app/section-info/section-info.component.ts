e111dbd6bb47802d55676edbd7f95fca8064de66
adeete <adeete09@gmail.com>
2020-11-10 10:54:46 +0530

    Fixed borders and about me section

--- src/app/section-info/section-info.component.ts
+++ src/app/section-info/section-info.component.ts
@@ -3,18 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-section-info',
   templateUrl: './section-info.component.html',
-  styleUrls: ['./section-info.component.scss']
+  styleUrls: ['./section-info.component.scss'],
 })
 export class SectionInfoComponent implements OnInit {
   oIntro: any;
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.oIntro = {
-      name: "Adeete",
-      occupations: ["Software Engineer", "Fullstack Developer"],
-      about: "I'm a 24 years old Full Stack Developer with 2 years of industry experience. I am a Computer Science Engineer currently living in Bengaluru, India. Have a look at my skills and experience or just connect with me on LinkedIn."
-    }
+      name: 'Adeete',
+      occupations: ['Software Engineer', 'Fullstack Developer'],
+      about:
+        "I'm a full stack developer, having obtained B.Tech degreee with Computer Science" 
+        +"Engineering as major, currently living in Bengaluru, India.Please have a look at my skills"
+        +"and experience and feel free to connect with me on Linkedin."
+    };
   }
-
 }
