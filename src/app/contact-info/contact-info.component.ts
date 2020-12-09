6d554d815ebda14df3d413a4172f4c408e30bd25
adeete <adeete09@gmail.com>
2020-12-09 11:35:00 +0530

    minor fixes

--- src/app/contact-info/contact-info.component.ts
+++ src/app/contact-info/contact-info.component.ts
@@ -15,24 +15,28 @@ export class ContactInfoComponent implements OnInit {
         iconType: 'gmailIcon',
         iconImg: '../../assets/img/gmail.png',
         contactType: 'Email Me',
+        target: '_blank',
         contactUrl: "mailto:adeete09@gmail.com",
       },
       {
         iconType: 'linkedinIcon',
         iconImg: '../../assets/img/linkedin.png',
         contactType: 'LinkedIn',
+        target: '_blank',
         contactUrl: "https://www.linkedin.com/in/adeete-kashyap-656b57126/",
       },
       {
         iconType: 'githubIcon',
         iconImg: '../../assets/img/github-logo.svg',
         contactType: 'Github',
+        target: '_blank',
         contactUrl: "https://github.com/adeete",
       },
       {
         iconType: 'resumeIcon',
         iconImg: '../../assets/img/down-arrow.png',
         contactType: 'Resume',
+        target: '_self',
         contactUrl: "https://drive.google.com/uc?export=download&id=16rh_hhLu--IgxHqPiiQJHV9slqwm7ev3",
       }
     ];
